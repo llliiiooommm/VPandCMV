7e8248e2a514d64b1513e8427112ae57cb91e455
Zaripov John <liomsky@yandex.ru>
2026-04-22 21:33:28 +0700

    Начало положено

---
+++ my_LICHNOE_WEATHER_PROBNIK/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>🌤️ Weather App</h1>
+      <p>Проект очищен. Готов к добавлению компонентов.</p>
+    </div>
+  )
+}
+
+export default App
